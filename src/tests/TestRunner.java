07fe4608744de99d56a0e1371c22af5d57812eb0
Nimendra Madampe <nimendramadampe93@gmail.com>
2017-10-11 00:04:30 +0530

    Added Test Runner Class to autmate testing

---
+++ src/tests/TestRunner.java
@@ -0,0 +1,34 @@
+package test;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+import tests.DiceValueTest;
+import tests.PlayerTest;
+
+public class TestRunner
+{
+    public static void main(String[] args)
+    {
+        Result result = JUnitCore.runClasses(DiceValueTest.class);
+        for(Failure failure : result.getFailures())
+        {
+            System.out.println(failure.toString());
+        }
+        System.out.println("DiceValue unit testing success: " + result.wasSuccessful());
+
+        result = JUnitCore.runClasses(PlayerTest.class);
+        for(Failure failure : result.getFailures())
+        {
+            System.out.println(failure.toString());
+        }
+        System.out.println("Player unit testing success: " + result.wasSuccessful());
+
+        result = JUnitCore.runClasses(test.GameTest.class);
+        for(Failure failure : result.getFailures())
+        {
+            System.out.println(failure.toString());
+        }
+        System.out.println("Game unit testing success: " + result.wasSuccessful());
+    }
+}
