2076f378089a5068a1d9ebe13f5c08fbb08d80d2
Nimendra Madampe <nimendramadampe93@gmail.com>
2017-10-10 17:31:03 +0530

    Fixed Some issues in Main.java and Player.java
    Added :
    
    Bug-4 Replication

---
+++ src/com/nimendra/BugReplications/Bug4Replication.java
@@ -0,0 +1,17 @@
+package com.nimendra.BugReplications;
+
+import com.nimendra.Main;
+
+import java.io.IOException;
+
+public class Bug4Replication {
+
+    public static void main(String[] args) throws IOException {
+
+        String[] argTest = {"4"};
+
+        Main.main(argTest);
+
+    }
+
+}
